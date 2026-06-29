cce934a4b3c489106448747861c47fd58d62b04f
root <root@ZodwaMtambo.localdomain>
2026-06-29 21:24:02 +0200

    Phase 2 complete: K-Means clustering returning dominant RGB colours

--- main.py
+++ main.py
@@ -2,12 +2,38 @@ import numpy as np
 from PIL import Image
 
 image = Image.open ("images/20221225_174227.jpg")
-
 image_array = np.array(image)
-print("Top-left pixel:", image_array[0,0])
-print("Center pixel:", image_array[1500,2000])
-print("Top-right:", image_array[0, 3999])
-print("Bottom-left:", image_array[2999, 0])
-print("Bottom-right:", image_array[2999, 3999])
-print("Array shape:", image_array.shape)
-print("Data type:", image_array.dtype)
+
+#phase 2 
+plain_image = Image.open ("images/Image 3.jpeg")
+plain_image_array = np.array(plain_image)
+
+
+image2= Image.open("images/Image 2.jpeg")
+image2_array = np.array (image2)
+
+
+image1= Image.open("images/Image 1.jpeg")
+image1_array = np.array (image1)
+
+
+print("plain image size:", plain_image_array.size)
+print("plain image shape:", plain_image_array.shape)
+print ("image2 size:", image2_array.size)
+print("image2 shape:", image2_array.shape)
+print ("image1 size:", image1_array.size)
+print("image1 shape:", image1_array.shape)
+
+#reshaping. 
+Ex_image = image2_array.reshape(-1,3)
+print ("plain picture 2D list:", Ex_image)
+print (" New shape", Ex_image.shape)
+
+#k-means
+from sklearn.cluster import KMeans
+
+model = KMeans (n_clusters= 3, random_state= 42)
+X = model.fit_predict(Ex_image)
+print(X)
+
+print (model.cluster_centers_.astype(int))
