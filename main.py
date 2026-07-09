d768e918e857107032cebf75f6868d72ce846672
root <root@ZodwaMtambo.localdomain>
2026-07-09 14:27:20 +0200

    Phase 3 complete: RGB to English colour naming using Euclidean distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import webcolors
 
 image = Image.open ("images/20221225_174227.jpg")
 image_array = np.array(image)
@@ -34,6 +35,29 @@ from sklearn.cluster import KMeans
 
 model = KMeans (n_clusters= 3, random_state= 42)
 X = model.fit_predict(Ex_image)
-print(X)
+print("cluster groups :", X)
 
-print (model.cluster_centers_.astype(int))
+dominant_colours = model.cluster_centers_.astype(int)
+print ("dominant colours are:", dominant_colours)
+
+# Phase3 
+for  cluster in dominant_colours:
+    min_distance = float("inf")
+    closest_name = None
+
+    for name in webcolors.names("css3"):
+        named_rgb = webcolors.name_to_rgb(name)
+
+        distance = np.sqrt(
+            (int(cluster[0]) - named_rgb.red) ** 2 +
+            (int(cluster[1]) - named_rgb.green) ** 2 +
+            (int(cluster[2]) - named_rgb.blue) ** 2
+        )
+            
+        if distance < min_distance:
+                min_distance = distance
+                closest_name = name
+        
+        colour_name = closest_name
+    
+    print(f"RGB {tuple(int(c) for c in cluster)} -> {closest_name}")
